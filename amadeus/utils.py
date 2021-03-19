ae69d7c4889b597853ad8dbe3b1ea0910120d3c2
komarp <pavel.komar.01@mail.ru>
2021-03-19 16:25:43 +0300

    attempt

--- amadeus/utils.py
+++ amadeus/utils.py
@@ -1,17 +1,39 @@
 import requests
 
 
-class APIFetcher:
+class OrionHealthAPI:
 
     def __init__(self, url, headers):
         self.url = url
         self.headers = headers
 
-    def get_response(self, form):
-        return requests.get(
+    def get_patient(self, surname, gender):
+        response = requests.get(
             self.url,
             params={
-                'gender': form.cleaned_data['gender'],
-                'family': form.cleaned_data['surname']
+                'gender': gender,
+                'family': surname
             },
             headers=self.headers)
+        if response.status_code == 200:
+            content = response.json()
+            if not content['total']:
+                patient_info_dict = {
+                    'status': 'undefined'
+                }
+            else:
+                patient_info = content['entry'][0]['resource']
+                patient_info_dict = {
+                    'status': 'success',
+                    'first_name': patient_info['name'][0]['given'][0],
+                    'last_name': patient_info['name'][0]['family'],
+                    'gender': patient_info['gender'],
+                    'birth_date': patient_info['birthDate'],
+                    'country': patient_info['address'][0]['country'],
+                    'city': patient_info['address'][0]['city'],
+                    'street': patient_info['address'][0]['line'][0],
+                    'postal_code': patient_info['address'][0]['postalCode']
+                }
+        else:
+            patient_info_dict = {'status': 'unsuccessful'}
+        return patient_info_dict
--- amadeus/views.py
+++ amadeus/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import FormView
 from .forms import RequestForm
-from .utils import APIFetcher
+from .utils import OrionHealthAPI
 import orion.settings as settings
 
 # Create your views here.
@@ -16,27 +16,17 @@ class PatientSearchView(FormView):
     }
 
     def form_valid(self, form):
-        response = APIFetcher(
+        gender = form.cleaned_data['gender']
+        surname = form.cleaned_data['surname']
+        patient = OrionHealthAPI(
             settings.API_URL,
             headers=self.headers).\
-            get_response(form)
-        if response.status_code == 200:
-            content = response.json()
-            if not content['total']:
-                result = render(self.request, 'amadeus/callback_page.html')
-            else:
-                patient_info = content['entry'][0]['resource']
-                patient_info_dict = {
-                    'first_name': patient_info['name'][0]['given'][0],
-                    'last_name': patient_info['name'][0]['family'],
-                    'gender': patient_info['gender'],
-                    'birth_date': patient_info['birthDate'],
-                    'country': patient_info['address'][0]['country'],
-                    'city': patient_info['address'][0]['city'],
-                    'street': patient_info['address'][0]['line'][0],
-                    'postal_code': patient_info['address'][0]['postalCode']
-                }
-                result = render(self.request, 'amadeus/patient_info.html', {'patient': patient_info_dict})
+            get_patient(surname, gender)
+        if patient['status'] == 'success':
+            context = {'patient': patient}
+            response = render(self.request, 'amadeus/patient_info.html', context)
+        elif patient['status'] == 'undefined':
+            response = render(self.request, 'amadeus/callback_page.html')
         else:
-            result = render(self.request, 'amadeus/unsuccessful_page.html')
-        return result
+            response = render(self.request, 'amadeus/unsuccessful_page.html')
+        return response
